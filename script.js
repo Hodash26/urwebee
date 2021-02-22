b9023a91892fe9b591735057e138235fa45dd964
TimmermanRemi <remi.timmerman.pro@gmail.com>
2021-02-22 16:02:58 +0100

    fix burger menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let burgerButton = document.querySelector('.burger--button');
+let aButton = document.querySelectorAll('.menu--off');
 let menuContent = document.querySelector('.main-menu');
 let logo = document.querySelector('.logo');
 let html = document.querySelector('html');
@@ -6,9 +7,22 @@ let home_link = document.querySelectorAll('.main-menu nav a');
 
 burgerButton.addEventListener('click', function(e) {
     e.preventDefault();
-    burgerButton.classList.toggle('active');
-    menuContent.classList.toggle('menu-active');
-    html.classList.toggle('main-menu-open');
-    logo.classList.toggle('none');
-
+    burgerButton.classList.toggle('active'); // bouton burger de on à off et de off à on
+    menuContent.classList.toggle('menu-active'); //ouvre la page de menu burger
+    html.classList.toggle('main-menu-open'); //empeche le scroll du menu burger
 });
+
+aButton.forEach(item => {
+  item.addEventListener('click',()=>{
+    burgerButton.classList.remove('active'); //remet le bouton burger en off
+    menuContent.classList.remove('menu-active'); //referme la page de menu burger
+    html.classList.remove('main-menu-open'); //autorise le scroll
+  });
+});
+
+window.addEventListener('scroll', function() {
+  console.log(window.pageYOffset + 'px');
+});
+
+Mobile : 850 - 1290
+Tablet : 850 - 1190
