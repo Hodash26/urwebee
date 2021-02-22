439e7975945ae32bdd6996554123e2ca3bc319e6
Florent <florent.bailly62400@gmail.com>
2021-02-22 16:04:55 +0100

    fix conflict

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let burgerButton = document.querySelector('.burger--button');
 let menuContent = document.querySelector('.main-menu');
-let logo = document.querySelector('.logo');
 let html = document.querySelector('html');
 let home_link = document.querySelectorAll('.main-menu nav a');
 
@@ -9,6 +8,4 @@ burgerButton.addEventListener('click', function(e) {
     burgerButton.classList.toggle('active');
     menuContent.classList.toggle('menu-active');
     html.classList.toggle('main-menu-open');
-    logo.classList.toggle('none');
-
 });
