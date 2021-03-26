5853adf2ff58c59808b363fded22cdd988e31d51
TimmermanRemi <remi.timmerman.pro@gmail.com>
2021-03-26 16:39:15 +0100

    adaptive color burger menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let burgerButton = document.querySelector('.burger--button');
+let burgerLine = document.querySelectorAll('.ligne');
 let aButton = document.querySelectorAll('.menu--off');
 let menuContent = document.querySelector('.main-menu');
 let html = document.querySelector('html');
@@ -7,6 +8,9 @@ let home_link = document.querySelectorAll('.main-menu nav a');
 burgerButton.addEventListener('click', function(e) {
     e.preventDefault();
     burgerButton.classList.toggle('active'); // bouton burger de on à off et de off à on
+    burgerLine.forEach((line) => {
+      line.style.background ="#fff";
+    });
     menuContent.classList.toggle('menu-active'); //ouvre la page de menu burger
     html.classList.toggle('main-menu-open'); //empeche le scroll du menu burger
 });
@@ -19,6 +23,21 @@ aButton.forEach(item => {
   });
 });
 
-window.addEventListener('scroll', function() {
-  console.log(window.pageYOffset + 'px');
+$(window).scroll(function() {
+  if (Array.prototype.some.call($('.white-header'), function(element) {
+    console.log("test");
+  	scrollPosition = $(window).scrollTop();
+    elementTop = $(element).offset().top;
+    elementBottom = $(element).outerHeight() + elementTop;
+    if (scrollPosition > elementTop && scrollPosition < elementBottom) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })) {
+  	$('.ligne').css('background', '#fff');
+  } else {
+  	$('.ligne').css('background', '#000');
+  }
 });
